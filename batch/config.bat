12177d126cd6c149456ef895c4fa84f88b011f94
Wise, Jason S <jswise@terracon.com>
2019-05-10 10:18:03 -0400

    Finalize conda installation

--- batch/config.bat
+++ batch/config.bat
@@ -1,30 +1,38 @@
+ECHO OFF
+
 SET version_number=0.0.0
+ECHO Version number: %version_number%
 
-REM Build the package in the default location.
-REM setlocal
-SET build_dir=%UserProfile%\AppData\Local\Continuum\miniconda3\conda-bld
+SET downloads_dir=%UserProfile%\Downloads
 
-REM Build the package in a custom location.
-REM SET build_dir=C:\temp\keene
+FOR %%a IN ("%~dp0\..\..\..") DO SET nearc_dir=%%~fa
+echo %nearc_dir%
 
-REM Define the package's file name using the version number.
-SET file_name=keenepyt-%version_number%-py36_0.tar.bz2
+REM REM Build the package in the default location.
+REM REM setlocal
+REM SET build_dir=%UserProfile%\AppData\Local\Continuum\miniconda3\conda-bld
 
-REM Define the conda Scripts directory, which depends on how things are installed.
-SET scripts_dir=%UserProfile%\AppData\Local\Continuum\miniconda3\Scripts
-SET conda_arcgis_user=%UserProfile%\AppData\Local\Programs\ArcGIS\Pro\bin\Python\Scripts
-SET conda_arcgis_default=C:\Program Files\ArcGIS\Pro\bin\Python\Scripts
-IF EXIST %conda_arcgis_user% SET scripts_dir=%conda_arcgis_user%
-IF EXIST %conda_arcgis_default% SET scripts_dir=%conda_arcgis_default%
+REM REM Build the package in a custom location.
+REM REM SET build_dir=C:\temp\keene
 
-REM Define the user's environments directory, which also depends on how things are installed.
-REM SET envs_dir=%UserProfile%\AppData\Local\Programs\ArcGIS\Pro\bin\Python\envs
-SET envs_dir=%UserProfile%\AppData\Local\ESRI\conda\envs
-REM SET envs_dir=%UserProfile%\AppData\Local\Continuum\miniconda3\envs
+REM REM Define the package's file name using the version number.
+REM SET file_name=keenepyt-%version_number%-py36_0.tar.bz2
 
-FOR %%a IN ("%~dp0\..") DO SET repo_dir=%%~fa
+REM REM Define the conda Scripts directory, which depends on how things are installed.
+REM SET scripts_dir=%UserProfile%\AppData\Local\Continuum\miniconda3\Scripts
+REM SET conda_arcgis_user=%UserProfile%\AppData\Local\Programs\ArcGIS\Pro\bin\Python\Scripts
+REM SET conda_arcgis_default=C:\Program Files\ArcGIS\Pro\bin\Python\Scripts
+REM IF EXIST %conda_arcgis_user% SET scripts_dir=%conda_arcgis_user%
+REM IF EXIST %conda_arcgis_default% SET scripts_dir=%conda_arcgis_default%
 
-REM REM Define channel directories (folders).
-REM SET dev_channel=\\phqWFS03\GISShare\conda\dev
-REM SET test_channel=\\phqWFS03\GISShare\conda\test
-REM SET prod_channel=\\phqWFS03\GISShare\conda\prod
+REM REM Define the user's environments directory, which also depends on how things are installed.
+REM REM SET envs_dir=%UserProfile%\AppData\Local\Programs\ArcGIS\Pro\bin\Python\envs
+REM SET envs_dir=%UserProfile%\AppData\Local\ESRI\conda\envs
+REM REM SET envs_dir=%UserProfile%\AppData\Local\Continuum\miniconda3\envs
+
+REM FOR %%a IN ("%~dp0\..") DO SET repo_dir=%%~fa
+
+REM REM REM Define channel directories (folders).
+REM REM SET dev_channel=\\phqWFS03\GISShare\conda\dev
+REM REM SET test_channel=\\phqWFS03\GISShare\conda\test
+REM REM SET prod_channel=\\phqWFS03\GISShare\conda\prod
--- batch/install-conda.bat
+++ batch/install-conda.bat
@@ -1,8 +1,10 @@
-REM Miniconda2-latest-Windows-x86_64.exe /InstallationType=JustMe /AddToPath=0 /RegisterPython=0 /S /D=P:\tools\miniconda2
-REM Miniconda3-latest-Windows-x86_64.exe /InstallationType=JustMe /AddToPath=0 /RegisterPython=0 /NoRegistry=1
-REM CD \Projects\NEARC\conda
-REM Miniconda3-latest-Windows-x86_64.exe /InstallationType=JustMe /AddToPath=0 /RegisterPython=0 /NoRegistry=1 /S /D=C:\Projects\NEARC\conda
-REM CD Scripts
-CALL conda activate root
+CALL config.bat
+ECHO ON
+
+REM %downloads_dir%\Miniconda3-latest-Windows-x86_64.exe /InstallationType=JustMe /AddToPath=0 /RegisterPython=0 /NoRegistry=1 /S /D=%nearc_dir%\conda
+CD %nearc_dir%\conda\Scripts
+CALL activate root
 CALL conda install -c msys2 m2-patch -y
 CALL conda install conda-build -y
+
+PAUSE
